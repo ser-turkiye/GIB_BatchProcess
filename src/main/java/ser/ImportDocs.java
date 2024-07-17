f9d8a571db3f58c632778c76ce519ced38717686
BBayhan <bulentbayhan@gmail.com>
2024-07-18 01:06:01 +0300

    log file eklendi

--- src/main/java/ser/ImportDocs.java
+++ src/main/java/ser/ImportDocs.java
@@ -12,6 +12,7 @@ import com.ser.blueline.modifiablemetadata.IArchiveFolderClassModifiable;
 import com.ser.foldermanager.IFolder;
 import com.ser.foldermanager.IFolderConnection;
 import de.ser.doxis4.agentserver.UnifiedAgent;
+import org.apache.commons.io.output.FileWriterWithEncoding;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.*;
@@ -42,9 +43,13 @@ public class ImportDocs extends UnifiedAgent {
     String DocumentType = "";
     String DocumentDate = "";
     String StoredDate = "";
+    String ProcessID;
+    String LogFilePath = "c:/tmp2/bulk/";
     IStringMatrix settingsMatrix = null;
     List<String> importLog = new ArrayList<>();
-
+    Integer cnt = 0;
+    Integer importedCount = 0;
+    Integer totalCount = 0;
 
     public ImportDocs() {
     }
@@ -57,8 +62,10 @@ public class ImportDocs extends UnifiedAgent {
             ses = getSes();
             srv = ses.getDocumentServer();
             settingsMatrix = getDocumentServer().getStringMatrix("GibFolderAllDocType", ses);
-            Integer cnt = 0;
+            ProcessID = UUID.randomUUID().toString();
+
             IDocument ldoc = this.getEventDocument();
+            File logFile = new File(LogFilePath + "GIB_IMPORT_LOG_" + ProcessID + ".txt");
 
             HashMap<Integer, String> flds = new HashMap();
             flds.put(0, "CIF");
@@ -70,7 +77,9 @@ public class ImportDocs extends UnifiedAgent {
             flds.put(6, "FilePath");
 
             try {
+                //addToLog(logFile,"Line1");
                 String excelPath = this.exportDocumentContent(ldoc, "C:/tmp2/bulk/import");
+
                 //String excelPath = "C:/tmp2/bulk/import/Migration04.xlsx";
                 FileInputStream fist = new FileInputStream(excelPath);
                 this.log.info("Exported excel file to path:" + excelPath);
@@ -82,7 +91,7 @@ public class ImportDocs extends UnifiedAgent {
                     flds.put(cnt, field);
                     cnt++;
                 }
-
+                totalCount = list.size() + 1;
                 Iterator var8 = list.entrySet().iterator();
                 String docDate, storedDate, docType, custType, filePath, rowNr;
                 this.log.info("Start second loop.");
@@ -93,7 +102,6 @@ public class ImportDocs extends UnifiedAgent {
                     rowNr = row.getCell(0).getStringCellValue().trim();
                     CIFNumber = row.getCell(1).getStringCellValue().trim();
                     log.info("Import Doc Info : " + CIFNumber);
-                    if(CIFNumber.isEmpty()){continue;}
                     AccountNumber = row.getCell(2).getStringCellValue().trim();
                     docType = row.getCell(3).getStringCellValue().trim();
                     docDate = row.getCell(4).getStringCellValue().trim();
@@ -102,14 +110,29 @@ public class ImportDocs extends UnifiedAgent {
                     filePath = row.getCell(7).getStringCellValue().trim();
                     CustomerType = (custType.equals("C") ? "corp" : "retail");
                     FileName = new File(filePath).getName();
+                    if(CIFNumber.isEmpty()){
+                        addToLog(logFile,"NOT IMPORTED (" + FileName + ") CIF NO IS EMPTY : " + CIFNumber);
+                        continue;
+                    }
+                    if(docType.isEmpty()){
+                        addToLog(logFile,"NOT IMPORTED (" + FileName + ") DOC TYPE IS EMPTY : " + docType);
+                        continue;
+                    }
+                    if(filePath.isEmpty()){
+                        addToLog(logFile,"NOT IMPORTED (" + FileName + ") FILE PATH IS EMPTY : " + filePath);
+                        continue;
+                    }
                     log.info("Import Doc Info : " + CIFNumber);
                     log.info("Import Doc Info : " + AccountNumber);
                     log.info("Import Doc Info : " + filePath);
                     log.info("Import Doc Info : " + FileName);
 
                     if(Objects.equals(FileName, "") || FileName.isEmpty()){
-                        throw new Exception("FileName is empty for filepath : " + filePath);
+                        addToLog(logFile,"NOT IMPORTED (" + FileName + ") FILENAME IS EMPTY : " + FileName);
+                        //throw new Exception("FileName is empty for filepath : " + filePath);
                     }
+
+                    addToLog(logFile,"IMPORT STARTED FOR (" + FileName + ")  : " + CIFNumber + " / " + AccountNumber);
 
                     Date date1 = new SimpleDateFormat("dd.MM.yyyy").parse(docDate);
                     DateFormat dt1 = new SimpleDateFormat("yyyyMMdd");
@@ -124,24 +147,25 @@ public class ImportDocs extends UnifiedAgent {
 
                     FolderName = getFolderNameFromGVList(docType,CustomerType);
                     if(Objects.equals(FolderName, "") || FolderName.isEmpty()){
-
+                        addToLog(logFile,"> NOT IMPORTED (" + FileName + ") FOLDER NAME NOT FOUND : " + FolderName);
+                        continue;
                         //throw new Exception("Folder not found for : " + docType);
-
                     }
                     DocumentType = getDocumentTypeFromGVList(docType,FolderName);
 
                     log.info("GIBDoc Import Doc found Class for Folder : " + FolderName);
                     String ClassID = getCategoryIDFromGVlist(FolderName);
                     if(Objects.equals(ClassID, "") || ClassID.isEmpty()){
-
+                        addToLog(logFile,"> NOT IMPORTED (" + FileName + ") CLASS NOT FOUND : " + ClassID);
+                        continue;
                         //throw new Exception("ClassID not found for name : " + FolderName);
-
                     }
                     log.info("GIBDoc Import Found Doc Class ID (" + ClassID + ") for : " + FolderName);
                     IDocument GIBDoc = null;
                     GIBDoc = getGIBDoc(ClassID,FileName);
                     if(GIBDoc != null){
                         log.info("GIB DOC EXIST : " + FileName);
+                        addToLog(logFile,"> ALREADY IMPORTED (" + FileName + ") : " + GIBDoc.getID());
                         continue;
                     }
                     IDocument doc = newFileToDocumentClass(filePath, ClassID);
@@ -153,20 +177,29 @@ public class ImportDocs extends UnifiedAgent {
                     doc.setDescriptorValue("_Title",FileName);
                     doc.setDescriptorValue("_DocumentType", DocumentType);
                     doc.commit();
-                    importLog.add("IMPORTED FOR : " + FolderName);
-                    importLog.add("IMPORTED FOR : " + docType);
-                    importLog.add("IMPORTED FOR : " + filePath);
+                    importedCount++;
+                    addToLog(logFile,"> IMPORTED (" + FileName + ") to " + CustomerType + " / " + FolderName);
                     log.info("GIBDoc Imported Doc ID : " + doc.getID());
-
                 }
                 this.log.info("Import GIBDoc from Excel Finished");
-                writeLogToFile(importLog, "importLog.txt");
+                addToLog(logFile,"IMPORTED DOCS [" + importedCount + " / " + totalCount + "]");
                 return this.resultSuccess("Ended successfully");
             } catch (Exception e) {
                 this.log.error("Exception Caught");
                 this.log.error(e.getMessage());
                 return resultError(e.getMessage());
             }
+        }
+    }
+    private void addToLog(File file, String log) throws Exception {
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(log);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        }catch (Exception e){
+            throw new Exception("Write error to log file : " + e);
         }
     }
     private void writeLogToFile(List<String> importLog, String fileName) {
